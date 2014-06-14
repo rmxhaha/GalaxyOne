bd14cace37341a168880448dd48f7ccfc083ded2
rmxhaha <rmxhaha@github.com>
2014-06-14 16:55:49 +0700

    client side tweaking and refactoring (too early canvas optimization is removed)

--- codex/client.js
+++ codex/client.js
@@ -24,6 +24,7 @@ function _extend( x, y ){
  *   -> message disappear after certain amount of time
  *   -> Chat Input only appear when enter is pressed then disappear again after message has been sent
  */
+ 
 //socket.emit( 'identification', prompt('Your Name : ') );
 socket.emit( 'identification', "Test Subject" );
 socket.on('identification', function( idnum ){
@@ -32,7 +33,7 @@ socket.on('identification', function( idnum ){
 
 function appendMessage( from, message ){
 	var newDiv = document.createElement("div");
-	newDiv.innerHTML = '<div class=chat-message><b>' + from + "</b> : " + message + "</div>";
+	newDiv.innerHTML = '<div class=chat-message><i>' + from + "</i> : " + message + "</div>";
 	var chatbox = document.getElementById("chat-container");
 	chatbox.insertBefore(newDiv,chatbox.lastChild);
 	chatbox.scrollTop = chatbox.scrollHeight;
@@ -44,69 +45,39 @@ socket.on('broadcast' ,function( data ){
 
 var chatinput = document.getElementById('chat-input');
 
-chatinput.onkeypress = function(e){
+chatinput.addEventListener("keypress", function(e){
 	if( e.keyCode == 13 ){
 		if( chatinput.value != '' ){
 			socket.emit('message', chatinput.value );
 			chatinput.value = '';
 		}
 	}
-};
+});
 
+window.addEventListener("keypress", function(e){
+	if( e.keyCode == 13 ){
+		if( chatinput.style.display == "none" ) {
+			chatinput.style.display = "block";
+			chatinput.focus();
+		} else 
+			chatinput.style.display = "none";
+	}
+});
 
 /** GalaxyOne Space battle
  *  Work :
  *   -> Interpolation
  */
-var gRadiusOn = false;
-var gDrawOptimizationOn = true;
 
-
-function drawViewCircle( units ){
-	
-	var fullCircle = Math.PI * 2;
-	
-	function angle(){
-		this.from = 0;
-		this.to = fullCircle;
-		
-		function cleanCut( from, to ){
-			
-		}
-		
-		this.cut = function( from, to ){
-			from = from % fullCircle;
-			to = to % fullCircle;
-			
-			if( from > to ){
-				var tmp = from;
-				from = to;
-				to = tmp;
-			}
-			
-			if( to - from ){
-				
-			}
-			
-		}
-	}
-	
-	
-	for( var i = 0; i < units.length; ++ i ){
-		var angle = new angle;
-		var unit = units[i];
-		var rad = viewRadius[ unit.type ];
-		var angle = angles[i];
-		
-		angles[i] = angle;
-		
-		
-	}
-	
-	
+function angle( entity ){
+	return Math.atan2( entity.vx, -entity.vy );
 }
  
+var gRadiusOn = false;
+
 var canvas = document.getElementById("canvas");
+var backgroundCanvas = document.getElementById("backgroundCanvas");
+
 var context = canvas.getContext("2d");
 
 var viewRadius = []; // get from server
@@ -125,10 +96,10 @@ function drawBackground( canvas ){
 }
 
 window.onresize = function( e ){
-	debugcanvas.width = canvas.width = window.innerWidth;
-	debugcanvas.height = canvas.height = window.innerHeight;
+	backgroundCanvas.width = canvas.width = window.innerWidth;
+	backgroundCanvas.height = canvas.height = window.innerHeight;
 
-	drawBackground( debugcanvas );
+	drawBackground( backgroundCanvas );
 }
 
 
@@ -144,30 +115,23 @@ var camera = {
 	planetSize : 300 
 };
 
-window.onmousemove = function (e) {
+window.addEventListener("mousemove", function (e) {
 	mx = e.pageX;
 	my = e.pageY;
-}
+});
 
-window.onkeypress = function (e) {
+window.addEventListener("keypress", function (e) {
 	if (e.keyCode == 32) {
 		gRadiusOn = true;
 	}
-	else if( e.keyCode == 13 ){
-		if( chatinput.style.display == "none" ) {
-			chatinput.style.display = "block";
-			chatinput.focus();
-		} else 
-			chatinput.style.display = "none";
-	}
-}
+});
 
-window.onkeyup = function(e){
+window.addEventListener("keyup", function(e){
 	if (e.keyCode == 32) {
 		gRadiusOn = false;
-		context.clearRect( 0, 0, canvas.width, canvas.height ); // FLAG : fast Path ( fix later )
 	}
-}
+	
+});
 
 // current dynamic data
 var cdata = {
@@ -190,8 +154,8 @@ socket.on('planet', function( data ){
 });
 
 
-var spaceshipImage = new Image();
-spaceshipImage.src = "/images/Models_M001_1.png";
+
+
 
 function ImageCache( image, x, y, width, height, cacheWidth, cacheHeight )
 {
@@ -205,101 +169,107 @@ function ImageCache( image, x, y, width, height, cacheWidth, cacheHeight )
 	this.cache = m_canvas;
 }
 
-var drawCaches = [];
+var drawShip = ( function(){
 
-function drawShip( options ) {
-	var _default = {
-		x : 0,
-		y : 0,
-		angle : 0,
-		size : 100,
-		type : 0,
-		legion : 0
-	};
+	// static variable
+	var spaceshipImage = new Image();
+	var drawCaches = [];
+
+	spaceshipImage.src = "/images/Models_M001_1.png";
+
 	
-	var setup = _extend( _default, options );
-
-	if( !(
-		camera.x < setup.x + camera.unitSize/2 &&
-		setup.x - camera.unitSize/2 < camera.x + canvas.width ||
-		camera.y < setup.y + camera.unitSize/2 &&
-		setup.y - camera.unitSize/2 < camera.y + canvas.height ) ) return false;
+	return function( options ){
+		var _default = {
+			x : 0,
+			y : 0,
+			angle : 0,
+			size : 100,
+			type : 0,
+			legion : 0
+		};
 		
-	for( var i in setup ){
-		if( setup.hasOwnProperty( i ) && i != 'angle' ){
-			setup[i] = Math.floor( setup[i] );
+		var setup = _extend( _default, options );
+
+		if( !(
+			camera.x < setup.x + camera.unitSize/2 &&
+			setup.x - camera.unitSize/2 < camera.x + canvas.width ||
+			camera.y < setup.y + camera.unitSize/2 &&
+			setup.y - camera.unitSize/2 < camera.y + canvas.height ) ) return false;
+			
+		for( var i in setup ){
+			if( setup.hasOwnProperty( i ) && i != 'angle' ){
+				setup[i] = Math.floor( setup[i] );
+			}
 		}
+		
+		if( typeof drawCaches[ setup.legion ] == 'undefined' ){
+			drawCaches[ setup.legion ] = [];
+		}
+		
+		if( typeof drawCaches[ setup.legion ][ setup.type ] == 'undefined' ){
+			drawCaches[ setup.legion ][ setup.type ] = new ImageCache( spaceshipImage, setup.type * 100, setup.legion * 100, 100, 100, camera.unitSize, camera.unitSize );
+		}
+		
+		var cache = drawCaches[ setup.legion ][ setup.type ].cache;
+		
+		context.save();
+		context.translate(setup.x, setup.y);
+		context.rotate( setup.angle);
+		context.drawImage(
+			cache, 		
+			-setup.size/2, 
+			-setup.size/2, 
+			setup.size, setup.size
+			);
+		context.restore();
+		
+		return true;
+		
+	}	
+})();
+
+var drawPlanet = ( function(){
+	var planetImage = new Image();
+	planetImage.src = "/images/Planets_M001.png";
+
+	var planetDrawCaches = [];
+	return function( options ){
+		var _default = {
+			x : 0,
+			y : 0,
+			type : 0,
+			radius : 150
+		}
+
+		var setup = _extend( _default, options );
+		
+		if( !(
+			camera.x < setup.x + camera.planetSize/2 &&
+			setup.x - camera.planetSize/2 < camera.x + canvas.width ||
+			camera.y < setup.y + camera.planetSize/2 &&
+			setup.y - camera.planetSize/2 < camera.y + canvas.height ) ) return false;
+		
+		
+		if( typeof planetDrawCaches[ setup.type ] == 'undefined' ){
+			planetDrawCaches[ setup.type ] = new ImageCache( planetImage, setup.type * 300, 0, 300, 300, camera.planetSize, camera.planetSize );
+		}
+		
+		var cache = planetDrawCaches[ setup.type ].cache;
+		
+		context.save();
+		context.translate(setup.x, setup.y);
+		context.drawImage(
+			cache, 		
+			-camera.planetSize/2, 
+			-camera.planetSize/2, 
+			camera.planetSize, camera.planetSize
+			);
+		context.restore();
+		 
+		return true;
 	}
-	
-	if( typeof drawCaches[ setup.legion ] == 'undefined' ){
-		drawCaches[ setup.legion ] = [];
-	}
-	
-	if( typeof drawCaches[ setup.legion ][ setup.type ] == 'undefined' ){
-		drawCaches[ setup.legion ][ setup.type ] = new ImageCache( spaceshipImage, setup.type * 100, setup.legion * 100, 100, 100, camera.unitSize, camera.unitSize );
-	}
-	
-	var cache = drawCaches[ setup.legion ][ setup.type ].cache;
-	
-	context.save();
-	context.translate(setup.x, setup.y);
-	context.rotate( setup.angle);
-	context.drawImage(
-		cache, 		
-		-setup.size/2, 
-		-setup.size/2, 
-		setup.size, setup.size
-		);
-	context.restore();
-	
-	return true;
-}
+})();
 
-var planetImage = new Image();
-planetImage.src = "/images/Planets_M001.png";
-
-var planetDrawCaches = [];
-
-
-function drawPlanet( options ){
-
-	var _default = {
-		x : 0,
-		y : 0,
-		type : 0
-	}
-
-	var setup = _extend( _default, options );
-	
-	if( !(
-		camera.x < setup.x + camera.planetSize/2 &&
-		setup.x - camera.planetSize/2 < camera.x + canvas.width ||
-		camera.y < setup.y + camera.planetSize/2 &&
-		setup.y - camera.planetSize/2 < camera.y + canvas.height ) ) return false;
-	
-	
-	if( typeof planetDrawCaches[ setup.type ] == 'undefined' ){
-		planetDrawCaches[ setup.type ] = new ImageCache( planetImage, setup.type * 300, 0, 300, 300, camera.planetSize, camera.planetSize );
-	}
-	
-	var cache = planetDrawCaches[ setup.type ].cache;
-	
-	context.save();
-	context.translate(setup.x, setup.y);
-	context.drawImage(
-		cache, 		
-		-camera.planetSize/2, 
-		-camera.planetSize/2, 
-		camera.planetSize, camera.planetSize
-		);
-	context.restore();
-	 
-	return true;
-}
-
-function angle( entity ){
-	return Math.atan2( entity.vx, -entity.vy );
-}
 
 var Time = function () {
 	this.time = new Date();
@@ -319,26 +289,14 @@ var Time = function () {
 
 var timer = new Time;
 
-// past data for clearing efficiently
-var pastRects = [];
-var pastPlanets = [];
-var pastCamera = camera;
-
-
-var c = 0;
-var t = 0;
-
 function mainloop(){
 	/* Interpolation */
 	var dt = timer.reset() / 1000;
 	
-	t += dt;
-	if( c ++ % 100 == 0 ) console.log( c / t, " FPS" );
-	
-	
+	// simulate velocity while waiting for next data correction to arrive
 	for( var i = 0 ; i < cdata.units.length; ++ i ){
 		var unit = cdata.units[i];
-				
+		
 		unit.x = unit.x + unit.vx * dt;
 		unit.y = unit.y + unit.vy * dt;
 	}
@@ -351,37 +309,13 @@ function mainloop(){
 	}
 	
 	
+	// mouse navigation to adjust camera position
 	if( mx < 20 ) camera.x -= 10;
 	if( mx > window.innerWidth - 20 ) camera.x += 10;
 	if( my < 20 ) camera.y -= 10;
 	if( my > window.innerHeight - 20 ) camera.y += 10;
 	
-	if( gRadiusOn ){
-		context.clearRect( 0,0, canvas.width,canvas.height );
-	}
-	else {
-		// clearing optimization 
-	
-		context.save();
-		context.translate( -pastCamera.x, -pastCamera.y );
-		var psize = pastCamera.unitSize;
-		for( var i = 0; i < pastRects.length; ++ i ){
-			var r = pastRects[i];
-
-			context.clearRect( r.x - psize, r.y - psize, psize * 2, psize * 2 ); 
-		}
-		
-		var psize = pastCamera.planetSize;
-		for( var i = 0; i < pastPlanets.length; ++ i ){
-			var p = pastPlanets[i];
-			context.clearRect( p.x - psize/2, p.y - psize/2, psize, psize );
-		}
-
-		pastRects = [];
-		pastPlanets = [];
-		context.restore();
-	}
-	
+	context.clearRect( 0,0, canvas.width,canvas.height );
 	
 	context.save();
 	context.translate( -camera.x, -camera.y );
@@ -404,58 +338,46 @@ function mainloop(){
 		var unit = cdata.units[i];
 		
 		// if drawn
-		if( drawShip({
-				x : unit.x,
-				y : unit.y,
-				angle : angle( unit ),
-				size : camera.unitSize,
-				type : unit.type,
-				legion : unit.legion
-			}))
-			{
-				pastRects.push( {x : unit.x, y : unit.y } );
-			}
+		drawShip({
+			x : unit.x,
+			y : unit.y,
+			angle : angle( unit ),
+			size : camera.unitSize,
+			type : unit.type,
+			legion : unit.legion
+		})
 	}
 	
 	for( var i = 0; i < cdata.heads.length; ++ i ){
 		var head = cdata.heads[i];
 		
 		// if drawn
-		if( drawShip({
-				x : head.x,
-				y : head.y,
-				angle : angle( head ),
-				size : camera.unitSize,
-				type : head.type,
-				legion : head.legion
-			}))
-			{
-				pastRects.push( {x : head.x, y : head.y} );				
-			}
-		
+		drawShip({
+			x : head.x,
+			y : head.y,
+			angle : angle( head ),
+			size : camera.unitSize,
+			type : head.type,
+			legion : head.legion
+		});
 	}
 
 
 	for( var i = 0; i < planets.length; ++ i ){
-		var p = planets[i];
-		if( drawPlanet( p ) ){
-			pastPlanets.push({ x : p.x, y : p.y});
-		}
+		drawPlanet( planets[i] );
 	}
 
 	context.restore();
-	
-	// storing current data for later ( clearing efficiently )
-	pastCamera = _extend( {}, camera );
-	
+		
 	requestAnimationFrame( mainloop );
 }
 
-window.onload = function(){
-	debugcanvas.width = canvas.width = window.innerWidth;
-	debugcanvas.height = canvas.height = window.innerHeight;
+window.addEventListener("load", function(){
+	backgroundCanvas.width = canvas.width = window.innerWidth;
+	backgroundCanvas.height = canvas.height = window.innerHeight;
 
-	drawBackground( debugcanvas );
+	drawBackground( backgroundCanvas );
 	
+	// start the never ending loop
 	mainloop();
-}
+});
